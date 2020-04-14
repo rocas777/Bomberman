5e53e45b82e0254ea88117e01fb3209f7b590f19
rocas777 <lmiguelapinto@gmail.com>
2020-04-14 21:35:40 +0100

    position classed filled

--- src/main/java/Position.java
+++ src/main/java/Position.java
@@ -1,2 +1,40 @@
+import java.util.Objects;
+
 public class Position {
+    private int x,y;
+
+    Position(int x,int y){
+        this.x=x;
+        this.y=y;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return x == position.x &&
+                y == position.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
 }
