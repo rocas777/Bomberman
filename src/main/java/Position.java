3dfdacb52b73d90212cb7c7f1527ef38008318d8
rocas777 <lmiguelapinto@gmail.com>
2020-04-14 21:47:06 +0100

    added error & max_x and max_y to position

--- src/main/java/Position.java
+++ src/main/java/Position.java
@@ -2,11 +2,26 @@ import java.util.Objects;
 
 public class Position {
     private int x,y;
+    private int x_max,y_max;
 
     Position(int x,int y){
-        this.x=x;
-        this.y=y;
+        this.x = x;
+        this.y = y;
     }
+
+    Position(int x,int y,int x_max,int y_max) throws PositionError{
+        this.x_max=x_max;
+        this.y_max=y_max;
+        if(x>x_max) {
+            throw new PositionError("x is bigger than x_max");
+        }
+        else if(y>y_max) {
+            throw new PositionError("y is bigger than y_max");
+        }
+        this.x = x;
+        this.y = y;
+    }
+
 
     public int getY() {
         return y;
@@ -16,13 +31,20 @@ public class Position {
         return x;
     }
 
-    public void setY(int y) {
-        this.y = y;
+    public void setY(int y) throws PositionError{
+        if(y>y_max)
+            throw new PositionError("y is bigger than y_max");
+        else
+            this.y = y;
     }
 
-    public void setX(int x) {
-        this.x = x;
+    public void setX(int x) throws PositionError {
+        if(x>x_max)
+            throw new PositionError("x is bigger than x_max");
+        else
+            this.x = x;
     }
+
 
     @Override
     public boolean equals(Object o) {
@@ -36,5 +58,15 @@ public class Position {
     @Override
     public int hashCode() {
         return Objects.hash(x, y);
+    }
+}
+
+class PositionError extends Exception{
+    private String error;
+    PositionError(String error) {
+        this.error=error;
+    }
+    public String toString(){
+        return ("Position Error Occurred: "+error) ;
     }
 }
