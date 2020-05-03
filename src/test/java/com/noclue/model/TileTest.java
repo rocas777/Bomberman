22e8a5cc5320663bc2c9253eacdff8095f50f30e
rocas777 <lmiguelapinto@gmail.com>
2020-05-03 02:47:56 +0100

    tile tests

--- src/test/java/com/noclue/model/TileTest.java
+++ src/test/java/com/noclue/model/TileTest.java
@@ -1,28 +1,62 @@
 package com.noclue.model;
 
+import com.noclue.Filler;
+import com.noclue.controller.Timer;
+import com.noclue.model.collectible.Collectible;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 public class TileTest {
+    Tile tile;
+    Position position;
+    Filler filler;
+    Filler f2;
+    Collectible collectible;
+    Collectible c2;
+    @Before
+    public void setup(){
+        position = Mockito.mock(Position.class);
+        filler = Mockito.mock(Filler.class);
+        f2 = Mockito.mock(Filler.class);
+        collectible = Mockito.mock(Collectible.class);
+        c2 =  Mockito.mock(Collectible.class);
+        tile = new Tile(position,collectible,filler);
+        when(filler.isFilled()).thenReturn(true);
+    }
 
     @Test
     public void getFiller() {
+        Assert.assertEquals(tile.getFiller(),filler);
     }
 
     @Test
     public void getCollectible() {
+        Assert.assertEquals(tile.getCollectible(),collectible);
     }
 
     @Test
     public void setCollectible() {
+        tile.setCollectible(c2);
+        Assert.assertEquals(tile.getCollectible(),c2);
+        tile.setCollectible(null);
+        Assert.assertEquals(tile.getCollectible(),null);
     }
 
     @Test
     public void setFiller() {
+        tile.setFiller(f2);
+        Assert.assertEquals(tile.getFiller(),f2);
+        tile.setFiller(null);
+        Assert.assertEquals(tile.getFiller(),null);
     }
 
     @Test
     public void isFilled() {
+        Assert.assertEquals(true,tile.isFilled());
     }
 }
